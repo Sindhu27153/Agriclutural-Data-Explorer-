9225a38bdfa3d9569909d22f579c7940749e5c46
sindhuja seenivasan <165896898+Sindhu27153@users.noreply.github.com>
2025-02-28 19:47:30 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import pymysql
+from sqlalchemy import create_engine
+from urllib.parse import quote_plus
+
+#details to connect to database
+host = '127.0.0.1'
+user = 'root'
+password = 'India@123'
+Database = 'Agrodata'
+
+
+# URL-encode the password
+encoded_password = quote_plus(password)
+
+#data
+agro_data_df = pd.read_csv("agro_data.csv")
+
+#connecting to Mysql
+
+connection = pymysql.connect(host=host, user=user, password=password)
+cursor = connection.cursor()
+cursor.execute(f"CREATE DATABASE IF NOT EXISTS {Database}")
+cursor.execute(f"use {Database}")
+connection.commit()
+# Connect to MySQL server using sqlalchemy create_engine with help of pymysql
+connection_url = f"mysql+pymysql://{user}:{encoded_password}@{host}/{Database}"
+
+engine = create_engine(connection_url)
+
+engine_connection = engine.connect()
+
+# Insert order DataFrame into SQL, creating table if it doesn't exist
+agro_data_df.to_sql('agrodata', con=engine_connection, if_exists='replace', index=False)
+
+print("table_formed")
+cursor.close()
+connection.close()
